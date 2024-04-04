8f45c9ef7fd90ed9e3622677950be1175744e753
mfahadjbr <mfahadjbr@gmail.com>
2024-04-04 14:59:53 -0700

    table

---
+++ main.ts
@@ -0,0 +1,14 @@
+#! /usr/env node
+import inquirer from "inquirer";
+const table1=await inquirer.prompt([
+    {
+        type:'number',
+        name:'fahad',
+        message:'Enter a table number = '
+    }
+])
+let i=1;
+while(i<=10){
+    console.log(`${table1.fahad} x ${i} = ${i*table1.fahad}`);
+    i++;
+}
